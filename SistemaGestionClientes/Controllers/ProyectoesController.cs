75e88e413e78c341e21b3a1c579bbcf379e97fdf
angelo23cft <angmaureira23@cftsa.cl>
2024-11-20 01:58:37 -0300

    Creacion de controllers y vistas

---
+++ SistemaGestionClientes/Controllers/ProyectoesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestionClientes.Data;
+using SistemaGestionClientes.Models;
+
+namespace SistemaGestionClientes.Controllers
+{
+    public class ProyectoesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProyectoesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Proyectoes
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Proyectos.Include(p => p.Cliente);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Proyectoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Proyectos == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyectos
+                .Include(p => p.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(proyecto);
+        }
+
+        // GET: Proyectoes/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.AgendaClientes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Proyectoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,ClienteId")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(proyecto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.AgendaClientes, "Id", "Id", proyecto.ClienteId);
+            return View(proyecto);
+        }
+
+        // GET: Proyectoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Proyectos == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.AgendaClientes, "Id", "Id", proyecto.ClienteId);
+            return View(proyecto);
+        }
+
+        // POST: Proyectoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,ClienteId")] Proyecto proyecto)
+        {
+            if (id != proyecto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(proyecto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProyectoExists(proyecto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.AgendaClientes, "Id", "Id", proyecto.ClienteId);
+            return View(proyecto);
+        }
+
+        // GET: Proyectoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Proyectos == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyectos
+                .Include(p => p.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(proyecto);
+        }
+
+        // POST: Proyectoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Proyectos == null)
+            {
+                return Problem("Entity set 'AppDbContext.Proyectos'  is null.");
+            }
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto != null)
+            {
+                _context.Proyectos.Remove(proyecto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProyectoExists(int id)
+        {
+          return (_context.Proyectos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
