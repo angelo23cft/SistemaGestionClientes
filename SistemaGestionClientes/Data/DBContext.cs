16fe497a78a2148484907700d0eae0a7ba75d1ff
angelo23cft <angmaureira23@cftsa.cl>
2024-11-20 01:08:11 -0300

    dbContext , modelo agendacliente, appsettings

---
+++ SistemaGestionClientes/Data/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestionClientes.Models;
+
+namespace SistemaGestionClientes.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<AgendaCliente> AgendaClientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=infolutions_agenda_cliente;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AgendaCliente>().ToTable("agenda_clientes");
+        }
+    }
+}
---
+++ SistemaGestionClientes/Models/AgendaCliente.cs
@@ -0,0 +1,18 @@
+﻿namespace SistemaGestionClientes.Models
+{
+    public class AgendaCliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Proyecto { get; set; }
+        public string Empresa { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaDeIngreso { get; set; }
+        public DateTime HoraDeAgenda { get; set; }
+        public DateTime? FechaHoraAtencionOficina { get; set; }
+        public bool Revisado { get; set; } // Indica si el cliente ha sido revisado
+        public string Comentarios { get; set; } // Comentarios de la atención
+        public int EquipoAtencion { get; set; } // Equipo que realizó la atención
+    }
+}
